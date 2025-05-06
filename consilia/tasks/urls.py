1fd7aa645bbd5479a5f8dfcec67220e28d01e6f1
Anastasia Demenkova <a.piatrovskaya@gmail.com>
2025-05-06 16:41:33 +0300

    feat: deleting of tasks

--- consilia/tasks/urls.py
+++ consilia/tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-task/', views.add_task, name='add_task'),
     path('task/<int:pk>', views.task_details, name='task_details'),
     path('task/edit/<int:pk>', views.edit_task, name='edit_task'),
+    path('task/delete/<int:pk>', views.delete_task, name='delete_task'),
 ]
--- consilia/tasks/views.py
+++ consilia/tasks/views.py
@@ -40,3 +40,12 @@ def edit_task(request, pk):
         form = TaskForm(instance=task)
 
     return render(request, 'tasks/edit_task.html', {'form': form, 'task': task})
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Tasks, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('index')
+    else:
+        return render(request, 'tasks/delete_task.html', {'task': task})
